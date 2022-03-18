c42d1d537c867976bf22cf9fa5ef3ee436bce0bd
Nikhil <nkarnwal07@gmail.com>
2022-03-18 20:30:51 +0530

    Setup the current user state in app.js and added logic on componentDidMount and componentWillUnmount

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from "react";
 import "./App.css";
 import Homepage from "./pages/homepage/Homepage";
 import { Switch, Route, useParams } from "react-router-dom";
 import Shop from "./pages/shop/Shop";
 import Header from "./components/header/Header";
 import SignInAndSignUp from "./pages/signinandsignup/SignInAndSignUp";
+import { auth } from "./firebase/firebase.utils";
+import { onAuthStateChanged } from "firebase/auth";
 
 const HatsPage = () => {
   const id = useParams();
@@ -15,17 +18,38 @@ const HatsPage = () => {
   );
 };
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/shop" component={Shop} />
-        <Route exact path="/signin" component={SignInAndSignUp} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      currentUser: null,
+    };
+  }
+
+  unsubscribeFromAuth = null;
+  componentDidMount() {
+    this.unsubscribeFromAuth = onAuthStateChanged((user) => {
+      this.setState({ currentUser: user });
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/shop" component={Shop} />
+          <Route exact path="/signin" component={SignInAndSignUp} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
